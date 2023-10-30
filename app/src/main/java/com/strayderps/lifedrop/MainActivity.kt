7be7689920e94df1ef0c797734ac87971ccc5309
David Strader <straderdave@gmail.com>
2023-10-29 18:49:35 -0700

    Test commit with obvious error in MainActivity.  To be reverted immediately.

--- app/src/main/java/com/strayderps/lifedrop/MainActivity.kt
+++ app/src/main/java/com/strayderps/lifedrop/MainActivity.kt
@@ -20,6 +20,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    INTENTIONAL ERROR TEST
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
