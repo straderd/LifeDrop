fdbbbf4c6a3bff4fa37727e4f94773fa3861b420
David Strader <straderdave@gmail.com>
2023-11-01 22:09:29 -0700

    Added Experimental println

--- app/src/main/java/com/strayderps/lifedrop/MainActivity.kt
+++ app/src/main/java/com/strayderps/lifedrop/MainActivity.kt
@@ -25,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        println("Experimental")
+
         // Health TextViews
         val tvPlayerOneHealth = binding.tvPlayerOneHealth
         val tvPlayerTwoHealth = binding.tvPlayerTwoHealth
